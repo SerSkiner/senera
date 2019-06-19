11b6b1af0df7e2a4e4b05a2f5c19c1917fb4f5ee
SerSkiner <serterion@gmail.com>
2019-06-19 16:23:46 +0300

    first commit

---
+++ src/main/java/com/ua/senera/SeneraApplication.java
@@ -0,0 +1,13 @@
+package com.ua.senera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeneraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeneraApplication.class, args);
+	}
+
+}
